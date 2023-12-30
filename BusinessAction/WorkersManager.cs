0aa9089e585587c43b2c289279423c9a740d30d6
Charlotte <thenicenightmare@gmail.com>
2023-12-29 20:57:41 -0500

    Agregar archivos de proyecto.

---
+++ BusinessAction/WorkersManager.cs
@@ -0,0 +1,35 @@
+﻿using SoapDani.BusinessMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace SoapDani.BusinessAction
+{
+   public static class WorkersManager
+
+    {
+        public WorkersResponse GetAllWorkers()
+        {
+
+            var workers = new List<Persona>()
+            {
+                new Persona()
+                {
+                    fullName = "Dani Olivo"
+                },
+                new Persona()
+                {
+                    fullName = "Marce Palacios"
+                }
+            };
+            return new WorkersResponse()
+            {
+
+                Workers = workers
+            };
+        }
+
+    }
+}
---
+++ BusinessMessage/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoapDani.BusinessMessage
+{
+    public class Persona
+    {
+        public string fullName { get; set; }
+
+
+
+    }
+}
